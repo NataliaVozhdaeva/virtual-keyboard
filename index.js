dcabb8b706787b87b0b52aa9b0538eb02d1a5acf
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-04-27 21:46:44 +0200

    feat: add linter to project

---
+++ index.js
@@ -0,0 +1,31 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+/**
+ * Implement class DepthCalculator with method calculateDepth
+ * that calculates deoth of nested array
+ *
+ * @example
+ *
+ * const depthCalc = new DepthCalculator();
+ * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
+ * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
+ * depthCalc.calculateDepth([[[]]]) => 3
+ *
+ */
+class DepthCalculator {
+  calculateDepth(arr) {
+    const total = 1;
+    let max = 0;
+
+    arr.forEach((el) => {
+      if (Array.isArray(el)) {
+        max = Math.max(this.calculateDepth(el), max);
+      }
+    });
+    return total + max;
+  }
+}
+
+module.exports = {
+  DepthCalculator,
+};
